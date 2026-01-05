d3dcf677b44fcb7320d36f11bb4ad9525675702f
yousif <ywsft30189@gmail.com>
2026-01-06 00:24:46 +0300

    complete Exercise-07
    Implemented five different counting scenarios using 'for' loops and the range function. The script demonstrates counting up, counting down, and using custom step increments (by 2 and 5) to master loop control flow.

---
+++ Some_Counting-07.py.py
@@ -0,0 +1,36 @@
+# count from 0 to 50 by 1
+print("-" * 26)
+print("Counting up from 0 to 50:")
+print("-" * 26)
+for i in range(0, 51):
+    print(i)
+print("------------------------------")
+# count from 50 down to 0 by 1
+print("-" * 26)
+print("counting down from 50 to 0:")
+print("-----------------------------")
+for i in range(50, -1, -1):
+    print(i)
+print("------------------------------")
+
+# count from 30 to 50 by 1  
+print("-" * 26)
+print("Counting up from 30 to 50:")
+print("-" * 26)
+for i in range(30, 51):
+    print(i)
+print("-----------------------------")
+# count from 50 down to 10 by 2
+print("-" * 26)
+print("Counting down from 50 to 10 by 2:")
+print("-" * 26)
+for i in range(50, 9, -2):
+    print(i)
+print("--------------------------")
+# count from 100 to 200 by 5
+print("-" * 26)
+print("counting up from 100 to 200 by 5:")
+print("-" * 26)
+for i in range(100, 201, 5):
+    print(i)
+print("-----------------------------------")   
