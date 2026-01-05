c115775106262b62058ebae11a31e04f0aef0cbe
yousif <ywsft30189@gmail.com>
2026-01-05 23:49:38 +0300

    complete exercise-02
    This script defines two integer variables (8 and 10), calculates their sum using the '+' operator, and prints the final result to the console

---
+++ Simple_Sums-02.py.py
@@ -0,0 +1,12 @@
+
+# Define the firstv variable with value 8
+first_number = 8
+
+# Define the second variable with value 10 
+second_number = 10
+
+# Calculate the sum of the two variables
+total = first_number + second_number
+
+# Print the result
+print("the total is:", total)
